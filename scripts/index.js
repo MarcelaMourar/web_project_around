0cd3743add40b3026617ca4e5488ff699ceff36b
MarcelaM <marcela_moura28@hotmail.com>
2025-04-24 17:52:42 +0200

    Nova atualização do projeto

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const inputJob = document.querySelector(".popup__input_job");
 inputName.value = name.textContent;
 inputJob.value = job.textContent;
 
+const popup = document.querySelector(".popup");
 const saveButton = document.querySelector(".popup__save-button");
 saveButton.addEventListener("click", saveName);
 function saveName() {
@@ -16,7 +17,6 @@ function saveName() {
 }
 
 const editButton = document.querySelector(".profile__edit-caneta");
-const popup = document.querySelector(".popup");
 
 editButton.addEventListener("click", function () {
   popup.classList.remove("hide-form");
@@ -26,4 +26,93 @@ const editIcon = document.querySelector(".popup__close-icon");
 
 editIcon.addEventListener("click", function () {
   popup.classList.add("hide-form");
+});
+
+const actionButton = document.querySelector(".profile__action-button");
+const popupAction = document.querySelector("#popup-action");
+const actionIcon = popupAction.querySelector(".popup__close-icon");
+
+actionButton.addEventListener("click", function () {
+  popupAction.classList.remove("hide-form");
+});
+
+actionIcon.addEventListener("click", function () {
+  popupAction.classList.add("hide-form");
+});
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+function criarCard(name, link) {
+  const template = document.querySelector("#card-template");
+  const cardElement = template.content.cloneNode(true);
+
+  const image = cardElement.querySelector(".element__image");
+  image.src = link;
+  image.alt = `Imagem de ${name}`;
+
+  const title = cardElement.querySelector(".element__title");
+  title.textContent = name;
+
+  return cardElement;
+}
+
+const container = document.querySelector(".elements");
+
+initialCards.forEach((card) => {
+  const novoCard = criarCard(card.name, card.link);
+  container.appendChild(novoCard);
+});
+
+const formNewCard = document.querySelector("#form-new-card");
+const inputTitle = formNewCard.querySelector("#title-input");
+const inputLink = formNewCard.querySelector("#link-input");
+const cardContainer = document.querySelector(".elements");
+
+formNewCard.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const titleValue = inputTitle.value;
+  const linkValue = inputLink.value;
+
+  const newCard = createCard(titleValue, linkValue);
+  cardContainer.prepend(newCard);
+
+  popupAction.classList.add("hide-form");
+});
+
+const popupImg = document.querySelector("#popup-img");
+const closePopupImg = popupImg.querySelector(".popup__close-icon");
+closePopupImg.addEventListener("click", function () {
+  popupImg.classList.remove("hide-form");
+});
+
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("element__trash")) {
+    e.target.closest(".element").remove();
+  }
 });
